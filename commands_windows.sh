e8e5a07e37c4aedb61ac01a0decf62e638ee499b
herify <herifyram@gmail.com>
2025-04-11 00:35:26 -0400

    resize ok, model placeholder ok

--- commands_windows.sh
+++ commands_windows.sh
@@ -2,5 +2,8 @@
 
 python -m venv myvenv &&
 ./myvenv/Scripts/pip install "fastapi[standard]" uvicorn &&
-./myvenv/Scripts/pip install keras
-./myvenv/Scripts/pip install tensorflow
+./myvenv/Scripts/pip install opencv-python &&
+./myvenv/Scripts/pip install numpy &&
+./myvenv/Scripts/pip install Pillow &&
+./myvenv/Scripts/pip install keras &&
+./myvenv/Scripts/pip install tensorflow 
